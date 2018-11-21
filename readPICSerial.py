88cd7aa2442a88984f411165c11740a58d7e2550
well <wellmend0@gmail.com>
2018-11-21 19:40:24 -0300

    add python script to read from pic serial

---
+++ readPICSerial.py
@@ -0,0 +1,36 @@
+#Run: sudo python3 readPICSerial
+#Requirements: pyserial, serial and Serial
+
+import serial
+from serial import Serial
+import array
+
+width   = 180
+height  = 55 #20x20 input image
+contW   = 0
+contH   = 0
+
+if __name__ == "__main__":
+    rx = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600, timeout = 0)
+    imageFile = open("accumulator.txt","w")
+    pixel = 0
+    condition = True
+    while condition:
+        pixel = rx.read(1)
+        if(pixel != b''):
+            if(contW==0 and contH==0):
+                print('Reading from PIC Serial...')
+            contW+=1
+            imageFile.write(str(ord(pixel)))
+            #print(ord(pixel))
+            if(contW==width):
+                imageFile.write("\n")
+                contW=0
+                contH+=1
+            else:
+                imageFile.write(" ")
+            if(contH==height):
+                condition = False
+
+    imageFile.close()
+    print('Image saved!')
